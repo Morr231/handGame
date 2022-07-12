6b711400d8ab010cfdae31f0f703957ba0be0e5f
Almaz <63015341+Morr231@users.noreply.github.com>
2022-07-12 19:52:55 +0600

    game added

---
+++ generator.js
@@ -0,0 +1,52 @@
+const logos = [
+    {
+        company: "Qazaq Republic",
+        img: "./logos/qr.png",
+    },
+    {
+        company: "Kaspi",
+        img: "./logos/kaspi.png",
+    },
+    {
+        company: "Apple",
+        img: "./logos/apple.png",
+    },
+    {
+        company: "Audi",
+        img: "./logos/audi.png",
+    },
+    {
+        company: "Kaz Munai Gaz",
+        img: "./logos/kmg.png",
+    },
+];
+
+const generatorImg = document.getElementById("generator-img");
+const generatorButton = document.getElementById("generator-button");
+const generatorCompany = document.getElementById("company");
+
+const nextButton = document.getElementById("next-button");
+
+let index = 0;
+generatorCompany.innerHTML = logos[index].company;
+
+generatorButton.addEventListener("click", () => {
+    generatorImg.style.display = "block";
+
+    generatorImg.setAttribute("src", logos[index].img);
+
+    generatorButton.style.display = "none";
+    nextButton.style.display = "block";
+});
+
+nextButton.addEventListener("click", () => {
+    console.log("Clicked");
+
+    generatorImg.style.display = "none";
+    index++;
+    generatorCompany.innerHTML = logos[index].company;
+    generatorButton.style.display = "block";
+    nextButton.style.display = "none";
+
+    console.log(index);
+});
--- index.js
+++ index.js
@@ -3,62 +3,49 @@ const canvas = document.getElementById("canvas");
 
 const resultCanvas = document.getElementById("result");
 const ctx = resultCanvas.getContext("2d");
-ctx.lineWidth = 3;
+ctx.lineWidth = 4;
 
 const context = canvas.getContext("2d");
 let trackButton = document.getElementById("trackbutton");
-// let updateNote = document.getElementById("updatenote");
-
 let isVideo = false;
 let model = null;
 
 const modelParams = {
-    flipHorizontal: true, // flip e.g for video
-    maxNumBoxes: 20, // maximum number of boxes to detect
-    iouThreshold: 0.5, // ioU threshold for non-max suppression
-    scoreThreshold: 0.6, // confidence threshold for predictions.
+    flipHorizontal: true,
+    maxNumBoxes: 20,
+    iouThreshold: 0.5,
+    scoreThreshold: 0.6,
 };
 
 function startVideo() {
     handTrack.startVideo(video).then(function (status) {
         console.log("video started", status);
         if (status) {
-            // updateNote.innerText = "Video started. Now tracking";
             isVideo = true;
             runDetection();
         } else {
-            // updateNote.innerText = "Please enable video";
         }
     });
 }
 
 function toggleVideo() {
     if (!isVideo) {
-        // updateNote.innerText = "Starting video";
         startVideo();
     } else {
-        // updateNote.innerText = "Stopping video";
         handTrack.stopVideo(video);
         isVideo = false;
-        // updateNote.innerText = "Video stopped";
     }
 }
 
-px = 0;
-py = 0;
+let px = 0;
+let py = 0;
 
 function runDetection() {
     model.detect(video).then((predictions) => {
-        // console.log("Predictions: ", predictions);
-
         predictions.forEach((el) => {
             console.log(el);
 
-            if (
-                el.label === "open" &&
-                px - el.bbox[0] < 50 &&
-                py - el.bbox[1] < 50
-            ) {
+            if (el.label === "open") {
                 ctx.beginPath();
 
                 if (px == 0 && py == 0) {
@@ -84,11 +71,7 @@ function runDetection() {
     });
 }
 
-// Load the model.
 handTrack.load(modelParams).then((lmodel) => {
-    // detect objects in the image.
-
     model = lmodel;
-    updateNote.innerText = "Loaded Model!";
     trackButton.disabled = false;
 });

5eb42aa64eb912d4553000a1dabf5a037ace73d2
Almaz <63015341+Morr231@users.noreply.github.com>
2022-07-13 08:15:36 +0600

    sort

--- sort/sort.js
+++ sort/sort.js
@@ -1,10 +1,70 @@
-const startGame = document.getElementById("start-game");
-const platform = document.getElementById("platform");
-const ball = document.getElementById("ball");
-const endGame = document.getElementById("end-game");
+const trashes = [
+    {
+        img: "./sort-img/trash-icon/bottle.png",
+        type: "glass",
+    },
+    {
+        img: "./sort-img/trash-icon/glass-of-water.png",
+        type: "glass",
+    },
+    {
+        img: "./sort-img/trash-icon/wine-glass.png",
+        type: "glass",
+    },
+    {
+        img: "./sort-img/trash-icon/paper.png",
+        type: "paper",
+    },
+    {
+        img: "./sort-img/trash-icon/paper (1).png",
+        type: "paper",
+    },
+    {
+        img: "./sort-img/trash-icon/paper-airplane.png",
+        type: "paper",
+    },
+    {
+        img: "./sort-img/trash-icon/plastic-bag.png",
+        type: "plastic",
+    },
+    {
+        img: "./sort-img/trash-icon/plastic-glass.png",
+        type: "plastic",
+    },
+    {
+        img: "./sort-img/trash-icon/water.png",
+        type: "plastic",
+    },
+];
+
+const trashFirst = document.getElementById("trash-1");
+const trashSecond = document.getElementById("trash-2");
+const trashThird = document.getElementById("trash-3");
+
+const trashContainerFirst = document.getElementById("trash-container-1");
+const trashContainerSecond = document.getElementById("trash-container-2");
+const trashContainerThird = document.getElementById("trash-container-3");
+
+const trashImg = [
+    { img: trashFirst, id: -1 },
+    { img: trashSecond, id: -1 },
+    { img: trashThird, id: -1 },
+];
+
+const randomNumbers = [
+    Math.floor(Math.random() * trashes.length),
+    Math.floor(Math.random() * trashes.length),
+    Math.floor(Math.random() * trashes.length),
+];
+
+randomNumbers.forEach((el, index) => {
+    trashImg[index].img.setAttribute("src", trashes[el].img);
+    trashImg[index].id = el;
+});
 
 const video = document.getElementById("myvideo");
 const canvas = document.getElementById("canvas");
+const cursor = document.getElementById("cursor");
 
 const context = canvas.getContext("2d");
 
@@ -38,17 +98,85 @@ function toggleVideo() {
     }
 }
 
-let px = 0;
-let py = 0;
-
-const timer = document.getElementById("timer");
-let timerCount = 0;
+function isCollide(a, b) {
+    return !(
+        a.y + a.height < b.y ||
+        a.y > b.y + b.height ||
+        a.x + a.width < b.x ||
+        a.x > b.x + b.width
+    );
+}
 
 function runDetection() {
     model.detect(video).then((predictions) => {
         predictions.forEach((el) => {
             if (el.label === "open") {
-                platform.style.top = `${2 * el.bbox[1]}px`;
+                cursor.setAttribute("src", "./sort-img/open.png");
+                cursor.style.left = `${3.5 * el.bbox[0]}px`;
+                cursor.style.top = `${3.5 * el.bbox[1]}px`;
+            } else if (el.label === "closed") {
+                cursor.setAttribute("src", "./sort-img/closed.png");
+                cursor.style.left = `${3.5 * el.bbox[0]}px`;
+                cursor.style.top = `${3.5 * el.bbox[1]}px`;
+
+                if (
+                    isCollide(
+                        trashImg[0].img.getBoundingClientRect(),
+                        cursor.getBoundingClientRect()
+                    )
+                ) {
+                    trashImg[0].img.style.left = cursor.style.left;
+                    trashImg[0].img.style.top = cursor.style.top;
+                } else if (
+                    isCollide(
+                        trashImg[1].img.getBoundingClientRect(),
+                        cursor.getBoundingClientRect()
+                    )
+                ) {
+                    trashImg[1].img.style.left = cursor.style.left;
+                    trashImg[1].img.style.top = cursor.style.top;
+                } else if (
+                    isCollide(
+                        trashImg[2].img.getBoundingClientRect(),
+                        cursor.getBoundingClientRect()
+                    )
+                ) {
+                    trashImg[2].img.style.left = cursor.style.left;
+                    trashImg[2].img.style.top = cursor.style.top;
+                }
+
+                trashImg.forEach((el) => {
+                    if (trashes[el.id].type === "plastic") {
+                        if (
+                            isCollide(
+                                el.img.getBoundingClientRect(),
+                                trashContainerFirst.getBoundingClientRect()
+                            )
+                        ) {
+                            el.img.style.display = "none";
+                        }
+                    }
+                    if (trashes[el.id].type === "glass") {
+                        if (
+                            isCollide(
+                                el.img.getBoundingClientRect(),
+                                trashContainerSecond.getBoundingClientRect()
+                            )
+                        ) {
+                            el.img.style.display = "none";
+                        }
+                    }
+                    if (trashes[el.id].type === "paper") {
+                        if (
+                            isCollide(
+                                el.img.getBoundingClientRect(),
+                                trashContainerThird.getBoundingClientRect()
+                            )
+                        ) {
+                            el.img.style.display = "none";
+                        }
+                    }
+                });
             }
         });
 
@@ -61,93 +189,4 @@ function runDetection() {
 
 handTrack.load(modelParams).then((lmodel) => {
     model = lmodel;
-});
-
-ball.style.right = "60px";
-ball.style.top = `${window.innerHeight / 2}px`;
-
-let rightInterval;
-let randomSide = Math.floor(Math.random() * 2);
-
-let side = "left";
-
-startGame.addEventListener("click", () => {
-    startGame.style.display = "none";
-
-    setInterval(() => {
-        let minute = `${Math.floor(timerCount / 60 / 10)}${Math.floor(
-            (timerCount / 60) % 10
-        )}`;
-        let sec = `${Math.floor((timerCount % 60) / 10)}${Math.floor(
-            (timerCount % 60) % 10
-        )}`;
-        timer.innerHTML = minute + ":" + sec;
-        timerCount++;
-    }, 1000);
-
-    console.log(ball.style.right);
-
-    rightInterval = setInterval(() => {
-        let ballCoords = ball.getBoundingClientRect();
-        let platformCoords = platform.getBoundingClientRect();
-
-        function isCollide(a, b) {
-            return !(
-                a.y + a.height < b.y ||
-                a.y > b.y + b.height ||
-                a.x + a.width < b.x ||
-                a.x > b.x + b.width
-            );
-        }
-
-        if (isCollide(ballCoords, platformCoords)) {
-            side = "left";
-        }
-
-        if (side === "left") {
-            ball.style.right = `${
-                parseInt(ball.style.right.split("p")[0]) + 7
-            }px`;
-        } else {
-            ball.style.right = `${
-                parseInt(ball.style.right.split("p")[0]) - 7
-            }px`;
-        }
-
-        if (randomSide == 0) {
-            ball.style.top = `${parseInt(ball.style.top.split("p")[0]) + 7}px`;
-        } else {
-            ball.style.top = `${parseInt(ball.style.top.split("p")[0]) - 7}px`;
-        }
-
-        if (ballCoords.left <= 10) {
-            if (side === "left") {
-                side = "right";
-            }
-        }
-
-        if (ballCoords.top <= 10) {
-            if (randomSide == 1) {
-                randomSide = 0;
-            }
-        }
-
-        if (ballCoords.bottom + 10 >= window.innerHeight) {
-            if (randomSide == 0) {
-                randomSide = 1;
-            }
-        }
-
-        if (ballCoords.right >= window.innerWidth - 10) {
-            let minute = `${Math.floor(timerCount / 60 / 10)}${Math.floor(
-                (timerCount / 60) % 10
-            )}`;
-            let sec = `${Math.floor((timerCount % 60) / 10)}${Math.floor(
-                (timerCount % 60) % 10
-            )}`;
-            endGame.innerHTML = `You survived ${minute + ":" + sec}`;
-            clearInterval(rightInterval);
-            startGame.style.display = "block";
-        }
-    }, 1);
 });
